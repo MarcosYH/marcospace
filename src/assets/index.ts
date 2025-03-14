de900815edfac43ee38002091c3468ff866af1ac
Marcos Y. <marcosdeimos18@gmail.com>
2025-03-14 19:33:10 +0100

    refactor : index for export path

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,19 @@
+import arrow from './icon/arrow.svg'
 import avatar from './avatar.png'
+import light from './light.png'
+import name from './name.png'
+import project1 from './project1.png'
+import project2 from './project2.png'
+import section from '../../public/bg.png'
 import sky from './sky.png'
 
 export const images = {
+    arrow,
     avatar,
+    light,
+    name,
+    section,
     sky,
+    project1,
+    project2
 }
---
+++ src/data/link.ts
@@ -0,0 +1,22 @@
+const socialLink = [
+    {
+      title: "Twitter X",
+      link: "https://x.com/marcosyehouessi",
+    },
+    {
+      title: "LinkdIn",
+      link: "https://www.linkedin.com/in/marcos-yehouessi-2a202a1b9/",
+    },
+    {
+      title: "Email",
+      link: "mailto:marcosyehouessi@outlook.fr",
+    },
+    {
+      title: "GitHub",
+      link: "https://github.com/MarcosYH",
+    },
+  ];
+
+  export const links ={
+    socialLink
+  }
