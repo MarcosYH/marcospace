2cb85712802914a3955c23de7554a4a0be16207f
Marcos Y. <marcosdeimos18@gmail.com>
2025-03-14 19:32:52 +0100

    refactor : home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Mounted from "@/components/Mounted";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main className="container mx-auto">{children}</main>
+        <Mounted>
+          <main className="">{children}</main>
+        </Mounted>
       </body>
     </html>
   );
